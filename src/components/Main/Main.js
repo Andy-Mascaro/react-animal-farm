5a7117d9c101838398734745e4b184a07f3b8cb0
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-03-07 16:48:40 -0800

    worked on animal.js and main.js. I just realized I did not commit all day.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import './Main.css';
 
+import background from '../../background.png';
+import { animals } from '../../data';
+import Animal from '../Animal/Animal';
+
+// export default function Main() {
+//   return (
+//     <div className="Main" style={{ backgroundImage: `url(${background})` }}>
+//       <main />
+//       {animals.map((item) => (<Animal key={item.name} {...item} />))}
+//     </div>
+
+//   );
+// }
+
 export default function Main() {
-  return <main></main>;
+  return (
+    <main style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((item) => (
+        <Animal 
+          key={item.name} {...item}
+          // type={item.type}
+          // name={item.name}
+          // says={item.says}
+          // top={item.top}
+          // left={item.left}
+        />
+      ))}
+    </main>
+
+  );
 }
